bf4a8ac1a59bf9d72585ae648afaed6c4c255fd9
Michael Knox <mike@rt.sysam.com.au>
2009-11-15 19:04:06 +1100

    Moved the tmp output file out of the working area, so git status doesn't get annoyed about it. (Also cleaned up the file)

--- testcases/test.sh
+++ testcases/test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 dir=`dirname $0`
+OUTFILE="/tmp/logparse.test.output"
 
 for testcase in `ls -dF1 $dir/* | grep '/$'`
 do
 	echo Testcase: $testcase
-	$1 -l $testcase/test.log -c $testcase/logparse.conf -d0 > tmp.output
-	diff -u tmp.output $testcase/expected.output
+	$1 -l $testcase/test.log -c $testcase/logparse.conf -d0 > $OUTFILE
+	diff -u $OUTFILE $testcase/expected.output
 	if test $? -eq 0 ; then
 		echo $testcase passed
 	else
 		echo $testcase failed
 	fi
+	rm $OUTFILE
 done
